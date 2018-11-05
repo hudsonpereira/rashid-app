461e6127e00fc4553f5dd53ca695b62bd54e0991
Hudson Pereira <hudson.byte@gmail.com>
2018-11-05 02:17:02 -0200

    party lvl component !

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <img class="my-5" alt="Rashid logo" src="./assets/rashid.png">
+
+    <div class="row">
+      <div class="col-md-9">
+        <div class="card bg-dark light">
+          <div class="card-body">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae modi nihil quisquam quasi consequatur, autem repellat quaerat officia dolorem id? Sequi, laboriosam iusto non magnam expedita vitae. Vitae, quam eum fugiat numquam accusamus nisi odit quo. Cumque assumenda, qui eligendi pariatur rem adipisci inventore voluptates, quo beatae obcaecati architecto officia eius, nisi a. Ratione fugit aut placeat veniam dolores odio ut iusto perspiciatis praesentium fuga, mollitia enim corporis facere nesciunt unde reprehenderit repellat amet, voluptate sequi repellendus, quidem recusandae. Minima, culpa. Ullam aliquam fugit perferendis ea doloribus, nihil deserunt dolores delectus quas corporis sed eligendi ipsam aperiam quam dolorem molestias?
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card bg-dark light">
+          <div class="card-header">Party Level Calculator</div>
+          <div class="card-body">
+
+            <PartyLevel />
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <footer class="my-5">
+      <p>Created with LOVE by Sonata Moonlight @ Gentebra</p>
+    </footer>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import PartyLevel from './components/PartyLevel/PartyLevel.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    PartyLevel
   }
 }
 </script>
@@ -22,7 +47,11 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+}
+
+body {
+  background: url('./assets/background.jpg');
+  background-size: cover;
 }
 </style>
---
+++ src/components/PartyLevel/PartyLevel.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <input class="form-control" type="text" placeholder="Enter the highest level" v-model="level">
+
+        <p class="my-3 text-justify" v-if="minPartyLevel">{{ minPartyLevel }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            level: null,
+            minPartyLevel: null,
+        }
+    },
+    watch: {
+        level: function(val) {
+            const level = parseInt(val);
+
+            if (Number.isInteger(level)) {
+                this.minPartyLevel = 'Min level is: ' + Math.round(val / 3 * 2)
+            } else {
+                this.minPartyLevel = null;
+            }
+        }
+    }
+}
+</script>
+
