f1d947702fb2d51ea6930f98f26ae1bd36c9ffa7
Hudson Pereira <hudson.byte@gmail.com>
2018-11-05 03:17:40 -0200

    rashid location switch

--- src/components/RashidLocation.vue
+++ src/components/RashidLocation.vue
@@ -10,9 +10,24 @@
 export default {
     computed: {
         location: () => {
-            const location = 'Svargrond';
+            const date = new Date();
 
-            return location;
+            switch(date.getDay()) {
+                case 1:
+                    return 'Svargrond';
+                case 2:
+                    return 'Liberty Bay';
+                case 3:
+                    return 'Port Hope';
+                case 4:
+                    return 'Ankrahmun';
+                case 5:
+                    return 'Darashia'
+                case 6:
+                    return 'Edron'
+                case 0:
+                    return 'Carlin';
+            }
         }
     }
 }
